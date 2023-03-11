449fa4f46020acdb78d9ce407da00d6004f380d2
Joel Matthews <joelmatthews.A@gmail.com>
2023-03-10 20:56:58 -0500

    add css styles to login page

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,59 +1,61 @@
-import { Form, json, redirect} from "react-router-dom";
-import axios from 'axios';
+import { Form, json, redirect } from "react-router-dom";
+import axios from "axios";
 
+import classes from './Login.module.css';
 
 const Login = () => {
-
   return (
-    <Form method="post" action="/">
-      <div>
-        <input type="email" name="email" />
-      </div>
-      <div>
-        <input type="password" name="password" />
-      </div>
-      <button>Login</button>
-    </Form>
+    <div className={classes['form-container']}>
+      <h3 className={classes['form-title']}>Login</h3>
+      <Form method="post" action="/">
+        <div className={classes['form-control']}>
+          <input type="email" name="email" placeholder="email" />
+        </div>
+        <div className={classes['form-control']}>
+          <input type="password" name="password" placeholder="password" />
+        </div>
+        <button className={classes['form-submit-btn']}>Authorize</button>
+      </Form>
+    </div>
   );
 };
 
-export async function action({request}) {
+export async function action({ request }) {
   const formData = await request.formData();
 
   const requestData = {
-    email: formData.get('email'),
-    password: formData.get('password')
+    email: formData.get("email"),
+    password: formData.get("password"),
   };
 
+  try {
+    const response = await axios({
+      method: "post",
+      url: "https://dev-api.zeroeyes.com/api/v1/Account/Login",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "text/plain",
+      },
+      data: requestData,
+    });
 
-    try {
-      const response = await axios({
-        method: 'post',
-        url: 'https://dev-api.zeroeyes.com/api/v1/Account/Login',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'text/plain'
-        },
-        data: requestData
-      });
-     
-      if (response.status === 200) {
+    if (response.status === 200) {
+      console.log(response.data);
 
-        console.log(response.data)
-
-        const accessToken = response.data.accesstoken;
-        localStorage.setItem('token', accessToken);
-        return redirect('/');
-        
-      }
-    } catch (error) {
-
-      if (error.response.status !== 400) {
-        throw json({message: error.response.data}, {status: error.response.status})
-      }
-
-      return error.response.data;
+      const accessToken = response.data.accesstoken;
+      localStorage.setItem("token", accessToken);
+      return redirect("/");
     }
-} 
+  } catch (error) {
+    if (error.response.status !== 400) {
+      throw json(
+        { message: error.response.data },
+        { status: error.response.status }
+      );
+    }
+
+    return error.response.data;
+  }
+}
 
 export default Login;
--- my-app/src/components/PageContent.js
+++ my-app/src/components/PageContent.js
@@ -3,7 +3,7 @@ import classes from './PageContent.module.css';
 const PageContent = ({ title, children }) => {
   return (
     <div className={classes.content}>
-      <h1>{title}</h1>
+      <h1 className={classes.title}>{title}</h1>
       {children}
     </div>
   );
