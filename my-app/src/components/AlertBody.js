0b01c1dc1473f68592b336640eabaeddbbedc953
Joel Matthews <joelmatthews.A@gmail.com>
2023-03-17 22:28:50 -0400

    Add canvas and drawing alert into canvas

--- my-app/src/components/AlertBody.js
+++ my-app/src/components/AlertBody.js
@@ -1,14 +1,20 @@
 import classes from "./AlertBody.module.css";
+import Canvas from "./Canvas";
 
 const AlertBody = ({ selectedAlert }) => {
+  const draw = (context) => {
+    const alertImage = new Image();
+    alertImage.src = selectedAlert.filename;
+    alertImage.id = selectedAlert.id;
+    alertImage.alt = `Alert: ${selectedAlert.id}`;
+
+    context.clearRect(0, 0, 640, 360);
+    context.drawImage(alertImage, 0, 0, 640, 360);
+  };
   return (
     <div className={classes.alertBody}>
       <h3>{`Alert:${selectedAlert.id} / Client: ${selectedAlert.client.name} / Camera: ${selectedAlert.camera.name}`}</h3>
-      <canvas
-        className={classes.alertCanvas}
-        height="360px"
-        width="640px"
-      ></canvas>
+      <Canvas width={"640px"} height={"360px"} draw={draw} />
     </div>
   );
 };
---
+++ my-app/src/components/Canvas.js
@@ -0,0 +1,14 @@
+import { useEffect, useRef } from "react";
+
+import classes from "./Canvas.module.css"
+
+const Canvas = ({ draw, height, width }) => {
+  const canvas = useRef();
+  useEffect(() => {
+      const context = canvas.current.getContext('2d');
+      draw(context);
+  }, [draw]);
+  return <canvas className={classes['alert-canvas']} ref={canvas} height={height} width={width}></canvas>;
+};
+
+export default Canvas;
